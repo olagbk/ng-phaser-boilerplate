665cd51a94f8018b94c30bf40541c6405cb07f6e
Aleksandra Gabka <aleksandra.gabka@boostcom.no>
2020-01-25 16:25:33 +0100

    add phaser

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+import { MainScene } from './main-scene';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [ MainScene ],
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 }
+        }
+      }
+    };
+  }
+  ngOnInit() {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
---
+++ src/app/game/main-scene.ts
@@ -0,0 +1,14 @@
+export class MainScene extends Phaser.Scene {
+    constructor() {
+      super({ key: 'main' });
+    }
+    create() {
+      console.log('create method');
+    }
+    preload() {
+      console.log('preload method');
+    }
+    update() {
+      console.log('update method');
+    }
+  }
